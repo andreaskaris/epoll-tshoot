cf6bd0d2bba98f4045a39a31cad678154fc19839
Andreas Karis <ak.karis@gmail.com>
2024-01-26 11:37:56 +0100

    next
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- systemtap/entrypoint.sh
+++ systemtap/entrypoint.sh
@@ -3,27 +3,17 @@ set -eux
 
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LOG_DIR=/host/var/log/systemtap
+STAP_LOG_MAX_SIZE=1048576
+LOG_ROTATE_SIZE=900M
+LOG_ROTATE_OLDER_THAN=5
 
-PGREP="${1}"
-if [ "${PGREP}" == "" ]; then
-    echo "Empty pgrep expression. Exiting." >&2
-    exit 1
-fi
+stap /systemtap/epoll_syscall.stap -o /host/var/log/systemtap/stap.log -S ${STAP_LOG_MAX_SIZE}
 
-if [ ! -f "${LOG_DIR}" ]; then
-    mkdir -p "${LOG_DIR}"
-fi
-
-pids=$(pgrep -f "${PGREP}")
-if [ $? != 0 ]; then
-    echo "Could not find process: '$var'. Skipping ..."
-    continue
-fi
-for pid in ${pids}; do
-    echo "" >> "${LOG_DIR}/${pid}.log"
-    echo "[$(date)] Monitoring pid ${pid}." | tee -a "${LOG_DIR}/${pid}.log"
-    stap -x ${pid} ${DIR}/epoll_syscall.stap >> "${LOG_DIR}/${pid}.log" 2>&1 &
+for i in {1..1440}; do
+    sleep 60
+    echo "Checking if logs need rotation ..."
+    pushd "${LOG_DIR}"
+        find . -maxdepth 1 -type f -size +${LOG_ROTATE_SIZE} -mmin +${LOG_ROTATE_OLDER_THAN} -exec gzip {} \;
+    popd
 done
-
-echo "Sleeping ..."
-/usr/bin/sleep infinity
+echo "Ran for a day, done."
